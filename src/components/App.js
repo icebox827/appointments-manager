702bd5a2cfb1acc3cfa23d86e0581423b430bd3f
Treasure Kabareebe <treasurecalm@gmail.com>
2020-11-15 04:23:51 +0300

    Add logic for sorting existing records

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+
 import AddAppointments from './AddAppointments';
-import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
 import { without } from 'lodash';
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -19,20 +20,28 @@ class App extends Component {
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
     this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
   }
 
-  toggleForm(){
+  toggleForm() {
     this.setState({
       formDisplay: !this.state.formDisplay
     });
   }
 
-  addAppointment(apt){
-    let temptApts = this.state.myAppointments;
-    apt.aptId = this.state.lastIndex;
-    temptApts.unshift(apt);
+  changeOrder(order, dir) {
     this.setState({
-      myAppointments: temptApts,
+      orderBy: order,
+      orderDir: dir
+    });
+  }
+
+  addAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
       lastIndex: this.state.lastIndex + 1
     });
   }
@@ -46,24 +55,22 @@ class App extends Component {
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('./data.json')
       .then(response => response.json())
       .then(result => {
         const apts = result.map(item => {
           item.aptId = this.state.lastIndex;
-          this.setState({lastIndex: this.state.lastIndex + 1})
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
           return item;
-        })
+        });
         this.setState({
           myAppointments: apts
         });
       });
-
   }
 
   render() {
-
     let order;
     let filteredApts = this.state.myAppointments;
     if (this.state.orderDir === 'asc') {
@@ -72,8 +79,11 @@ class App extends Component {
       order = -1;
     }
 
-    filteredApts.sort((a,b) => {
-      if (a[this.state.orderBy].toLowerCase() < b[this.state.orderBy].toLowerCase()) {
+    filteredApts.sort((a, b) => {
+      if (
+        a[this.state.orderBy].toLowerCase() <
+        b[this.state.orderBy].toLowerCase()
+      ) {
         return -1 * order;
       } else {
         return 1 * order;
@@ -86,18 +96,20 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments 
+                <AddAppointments
                   formDisplay={this.state.formDisplay}
                   toggleForm={this.toggleForm}
                   addAppointment={this.addAppointment}
                 />
-                <SearchAppointments 
+                <SearchAppointments
                   orderBy={this.state.orderBy}
                   orderDir={this.state.orderDir}
+                  changeOrder={this.changeOrder}
                 />
-                <ListAppointments 
-                  appointments={filteredApts} 
-                  deleteAppointment={this.deleteAppointment} />
+                <ListAppointments
+                  appointments={filteredApts}
+                  deleteAppointment={this.deleteAppointment}
+                />
               </div>
             </div>
           </div>
